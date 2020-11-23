fd3914ed34e9f4d74c12ae08e78ad4a98c29b381
Gustavo Adrian Marino <gamarino@numaes.com>
2020-11-23 19:41:41 -0300

    [FIX] cleaning

--- niova_invoice_scan/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#################################################################################
-# Author      : Niova Group ApS (<https://niova.dk/>)
-# Copyright(c): 2018-Present Niova Group ApS
-# License URL : https://invoice-scan.com/license/
-# All Rights Reserved.
-#################################################################################
-from . import models
-from . import wizard
--- tls_tollsync/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_config_settings
-from . import tls_toll_provider
-from . import fleet_vehicle
-from . import tls_tools
-from . import tls_toll
