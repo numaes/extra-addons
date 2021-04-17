ee1b6c3c7b2bd86b8a404fcb112b332e8c4ba74f
Gustavo Adrian Marino <gamarino@numaes.com>
2021-04-17 20:01:04 -0300

    [FIX] base_accounting_kit: raises on wrong fields

--- base_accounting_kit/models/account_move.py
+++ base_accounting_kit/models/account_move.py
@@ -140,9 +140,9 @@ class AccountInvoiceLine(models.Model):
 
     @api.onchange('asset_category_id')
     def onchange_asset_category_id(self):
-        if self.move_id == 'out_invoice' and self.asset_category_id:
+        if self.move_id.move_type == 'out_invoice' and self.asset_category_id:
             self.account_id = self.asset_category_id.account_asset_id.id
-        elif self.move_id == 'in_invoice' and self.asset_category_id:
+        elif self.move_id.move_type == 'in_invoice' and self.asset_category_id:
             self.account_id = self.asset_category_id.account_asset_id.id
 
     @api.onchange('product_uom_id')
@@ -155,17 +155,17 @@ class AccountInvoiceLine(models.Model):
     def _onchange_product_id(self):
         vals = super(AccountInvoiceLine, self)._onchange_product_id()
         if self.product_id:
-            if self.move_id == 'out_invoice':
+            if self.move_id.move_type == 'out_invoice':
                 self.asset_category_id = self.product_id.product_tmpl_id.deferred_revenue_category_id
-            elif self.move_id == 'in_invoice':
+            elif self.move_id.move_type == 'in_invoice':
                 self.asset_category_id = self.product_id.product_tmpl_id.asset_category_id
         return vals
 
     def _set_additional_fields(self, invoice):
         if not self.asset_category_id:
-            if invoice.type == 'out_invoice':
+            if invoice.move_type == 'out_invoice':
                 self.asset_category_id = self.product_id.product_tmpl_id.deferred_revenue_category_id.id
-            elif invoice.type == 'in_invoice':
+            elif invoice.move_type == 'in_invoice':
                 self.asset_category_id = self.product_id.product_tmpl_id.asset_category_id.id
             self.onchange_asset_category_id()
         super(AccountInvoiceLine, self)._set_additional_fields(invoice)
