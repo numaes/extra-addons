5d4f8b382308de8bf351190c4790b66afcc4efba
luisprivado <luisprivado@hotmail.com>
2021-10-16 14:07:09 -0300

    [ACT] account_bank_statement_import version to 14.0.2.4

--- om_account_bank_statement_import/__manifest__.py
+++ om_account_bank_statement_import/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Odoo 14 Account Bank Statement Import',
-    'version': '14.0.1.0.0',
+    'version': '14.0.2.4.0',
     'category': 'Accounting',
     'depends': ['account'],
     'website': 'https://www.odoomates.tech',
@@ -11,13 +11,11 @@
     'maintainer': 'Odoo Mates',
     'license': 'LGPL-3',
     'description': """Generic Wizard to Import Bank Statements In Odoo14 Community Edition.
-(This module does not include any type of import format.)
-OFX and QIF imports are available in Enterprise version.""",
+(This module does include any CSV and XLSX type import format.)""",
     'data': [
         'security/ir.model.access.csv',
-        'account_bank_statement_import_view.xml',
-        'account_import_tip_data.xml',
         'wizard/journal_creation.xml',
+        'views/account_bank_statement_import_view.xml',
         'views/account_bank_statement_import_templates.xml',
     ],
     'demo': [
