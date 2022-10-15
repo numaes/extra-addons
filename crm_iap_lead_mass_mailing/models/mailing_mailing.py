e97fdc5b487faff98f0cfbe2a8c2d3f000e093a1
Gustavo Adrian Marino <gamarino@numaes.com>
2022-10-15 09:41:39 -0300

    [INIT] Clean up for initial 16.0

--- crm_iap_lead_mass_mailing/models/mailing_mailing.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class MailingMailing(models.Model):
-
-    _inherit = 'mailing.mailing'
-
-    create_lead_open = fields.Boolean(string='Create Lead/Opportunity '
-                                             'on mail open')
